88dfec367b1d6ca50226c552048dc537c75addfd
awtrix <3075814634@qq.com>
2022-06-27 22:55:23 +0800

    移植rtt

--- RT1064_RTT/my-smartcar_rtt/Project/CODE/motor.c
+++ RT1064_RTT/my-smartcar_rtt/Project/CODE/motor.c
@@ -1,18 +1,24 @@
 #include "motor.h"
 
-#define DIR_L				D0
-#define DIR_R				D2
-
-#define PWM_L				PWM1_MODULE3_CHB_D1
-#define PWM_R				PWM2_MODULE3_CHB_D3
+#define DIR_1 D14
+#define DIR_2 D0
+#define DIR_3 D1
+#define DIR_4 D15
+#define PWM_1 PWM1_MODULE0_CHA_D12
+#define PWM_2 PWM2_MODULE3_CHA_D2
+#define PWM_3 PWM2_MODULE3_CHB_D3
+#define PWM_4 PWM1_MODULE0_CHB_D13
 
 void motor_init(void)
 {
-	gpio_init(DIR_L, GPO, GPIO_HIGH, GPIO_PIN_CONFIG);
-	gpio_init(DIR_R, GPO, GPIO_HIGH, GPIO_PIN_CONFIG);
-
-	pwm_init(PWM_L, 10000, 0);												// PWM 通道2 初始化频率10KHz 占空比初始为0
-	pwm_init(PWM_R, 10000, 0);												// PWM 通道4 初始化频率10KHz 占空比初始为0
+    gpio_init(DIR_1, GPO, 0, GPIO_PIN_CONFIG); 		//单片机端口D0 初始化DIR_1			GPIO
+    gpio_init(DIR_2, GPO, 0, GPIO_PIN_CONFIG); 		//单片机端口D1 初始化DIR_2			GPIO
+    pwm_init(PWM_1, 17000, 0);      							//单片机端口D2 初始化PWM_1周期10K 占空比0
+    pwm_init(PWM_2, 17000, 0);     								//单片机端口D3 初始化PWM_2周期10K 占空比0
+    gpio_init(DIR_3, GPO, 0, GPIO_PIN_CONFIG);     //单片机端口D0 初始化DIR_1          GPIO
+    gpio_init(DIR_4, GPO, 0, GPIO_PIN_CONFIG);     //单片机端口D1 初始化DIR_2          GPIO
+    pwm_init(PWM_3, 17000, 0);                     //单片机端口D2 初始化PWM_1周期10K 占空比0
+    pwm_init(PWM_4, 17000, 0);                     //单片机端口D3 初始化PWM_2周期10K 占空比0											// PWM 通道4 初始化频率10KHz 占空比初始为0
 }
 
 void motor_pid(int16 expect_speed)
